a6d9dd4eb5729f074930a8a11ad7fb0f608c6694
VaibhavDabral11 <vaibhavdabral44@gmail.com>
2023-01-20 19:26:41 +0530

    fetch data from api

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
+const axios = require('axios')
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type post{
+    userId: Int
+    id: Int
+    title : String
+    body: String
+  }
+  type User {
+     name: String
+     age: Int
+     collage: String
+  }
+
   type Query {
     hello: String!
     howareyou : String
     WelcomeMessage(name: String): String
+    getUser: User
+    getpostsFromExternalAPI: [post ] 
   }
 `);
 
@@ -21,6 +36,18 @@ var root = {
     },
     WelcomeMessage: args => {
         return `Hey ${args.name}, hows life goes on `;
+    },
+    getUser: () => {
+        const user = {
+            name: "Harsh Shina",
+            age: 26,
+            collage: "IIT Guwahati",
+        };
+        return user;
+    },
+    getpostsFromExternalAPI: async() => {
+        const result = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        return result.data
     }
 };
 
